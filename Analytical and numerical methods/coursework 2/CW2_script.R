676eef472000b7ef220831e601a6b7cc8617ffed
“Ale”-cianfa <alessandra.cianfanelli@gmail.com>
2021-11-06 17:06:02 +0000

    ale adding graphs and comments

--- Analytical and numerical methods/coursework 2/CW2_script.R
+++ Analytical and numerical methods/coursework 2/CW2_script.R
@@ -1,30 +1,41 @@
-# Coursework 2 script 
+# DAN Coursework 2 script for graphs
+# Candidate number: rvzk6
+# dzta: 8th November 2021
 
-#Packages 
-library(tidyverse)
+## Loading Packages--- 
+library(tidyverse) #this contains the ggplot package for graphing 
+library(ggstatsplot)
+library(palmerpenguins)
 
-# Loading the Datasets 
-primary <- read.csv("Analytical and numerical methods/coursework 2/Dan cw2 primary.csv") #csv file with all the traits
-secondary <- read.csv("Analytical and numerical methods/coursework 2/Dan cw2 secondary.csv") #csv file with all the traits
+## Loading the Datasets and basic data wrangling----
+primary <- read.csv("Analytical and numerical methods/coursework 2/Dan cw2 primary.csv")
+  #^primary dataset after outlier removal
+secondary <- read.csv("Analytical and numerical methods/coursework 2/Dan cw2 secondary.csv") 
+  #^secondary dataset after outlier removal
 
-primary$df <- "Primary"
+primary$df <- "Primary" #adding a column that describes what dataframe (df) the data belongs to 
 
-secondary$df <- "Secondary"
+secondary$df <- "Secondary" #doing the same thing for the secondary dataset
 
-total <- rbind(primary, secondary)
+total <- rbind(primary, secondary) #creating a final dataset including both primary and seocndary data
 
+## Plottig----
 source("https://gist.githubusercontent.com/benmarwick/2a1bb0133ff568cbe28d/raw/fb53bd97121f7f9ce947837ef1a4c65a73bffb3f/geom_flat_violin.R")
+#this is the source necessary for the violin box plots (found trough the our coding club website tutorial, here:https://ourcodingclub.github.io/tutorials/dataviz-beautification-synthesis/)
 
-(violin <- ggplot(total, aes(df, MSL, group = df, fill = df)) + 
-    geom_flat_violin(position = position_nudge(x = 0, y = 0), alpha = 0.4) + #The half violins
-    geom_boxplot(width = 0.4, alpha = 1) + #The boxplots
+(violin_sp <- ggplot(total, aes(df, MSL, group = df, fill = df)) + 
+    geom_flat_violin(position = position_nudge(x = 0.15, y = 0), alpha = 0.4) + #The half violins
+    geom_boxplot(width = 0.2, alpha = 1) + #The boxplots
     labs(y = "Mean Sea Level (m)", x = "\nCoordinate set", fill = "Coordinate set") +
     scale_fill_manual(values = c("#D690FF", "#9BC2E6")) +
+    scale_colour_manual(values = c("darkorchid3", "#104E8B")) +
     coord_flip() +
     theme(axis.text.x = element_text(angle = 23)) +
+    guides(color = "none") +
     theme_bw() + #without thegridline is theme_classic
-    theme(axis.text = element_text(size = 12),
-          axis.title = element_text(size = 14, face = "plain")))
+    theme(axis.text = element_text(size = 10),
+          axis.title = element_text(size = 13, face = "plain")))
+ggsave(violin_sp, file = "Analytical and numerical methods/coursework 2/img/P_S_boxplot.png", height = 4, width = 6)
 
 #Density plot: 
   (Deansity_p <- ggplot(primary, aes(x=MSL)) +
@@ -34,6 +45,8 @@ source("https://gist.githubusercontent.com/benmarwick/2a1bb0133ff568cbe28d/raw/f
   theme(axis.text = element_text(size = 12),
         axis.title = element_text(size = 14, face = "plain")))
 
+#ggsave(Deansity_p, file = "Analytical and numerical methods/coursework 2/img/distribution_p.png", height = 4, width = 6)
+
 
 (Deansity_s <- ggplot(secondary, aes(x=MSL)) +
     geom_density(fill="#9BC2E6", color="#00688B", alpha=0.8) +
@@ -41,3 +54,4 @@ source("https://gist.githubusercontent.com/benmarwick/2a1bb0133ff568cbe28d/raw/f
     theme_classic() +
     theme(axis.text = element_text(size = 12),
           axis.title = element_text(size = 14, face = "plain")))
+#ggsave(Deansity_s, file = "Analytical and numerical methods/coursework 2/img/distribution_p.png", height = 4, width = 6)
